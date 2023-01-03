efd746fc30e5a2d84c605e017a2bf9c4e03ff02e
kiplaacollo <kiplaacollo@gmail.com>
2023-01-03 16:22:14 +0300

    Add changes to css, html and index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-nav");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("is-active");
+});
+mobileMenu.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("is-active");
+});
+document.querySelectorAll('.nav-item').forEach((n)=> 
+n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    mobileMenu.classList.remove('is-active');
+})
+);
