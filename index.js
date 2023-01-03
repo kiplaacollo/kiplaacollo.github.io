c0d61cd7067a3c7fe1b3dd0f8aa916d859493863
kiplaacollo <kiplaacollo@gmail.com>
2023-01-03 20:06:40 +0300

    Fix linters errors

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
-const mobileMenu = document.querySelector(".mobile-nav");
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobileMenu.classList.toggle("is-active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobileMenu.classList.toggle('is-active');
 });
-mobileMenu.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobileMenu.classList.toggle("is-active");
+mobileMenu.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobileMenu.classList.toggle('is-active');
 });
-document.querySelectorAll('.nav-item').forEach((n)=> 
-n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    mobileMenu.classList.remove('is-active');
-})
-);
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  mobileMenu.classList.remove('is-active');
+}));
