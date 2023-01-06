ba43bf6aac18f3d589d465a0ea6ef547d8e7f1b4
kiplaacollo <kiplaacollo@gmail.com>
2023-01-06 09:26:20 +0300

    Update CSS and Js to fix errors

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
   mobileMenu.classList.remove('is-active');
 }));
 
-//Popup details
+// Popup details
 const objWorks = {
   title: 'Keeping track of hundreds of components',
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: '<img>images/projet1.png</img>',
+  image: '',
   technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
   linkSource: 'https//www.githubrepo',
   linkLive: 'https//www.gitpages',
-}
+};
 const popup = document.getElementById('popup-window');
-//get button that opens the popup
+// get button that opens the popup
 const btn = document.getElementById('button');
-//get span element that closes popup
-const span = document.getElementsByClassName('close')[0];
-//to open the button on click
-btn.addEventListener ('click', () => {
+// get span element that closes popup
+const span = document.getElementsById('close')[0];
+// to open the button on click
+btn.addEventListener('click', () => {
   const title = document.getElementById('projectTitle');
   title.innerHTML = objWorks.title;
   const description = document.getElementById('projectDescription');
@@ -43,33 +43,33 @@ btn.addEventListener ('click', () => {
   button.innerHTML = 'See live <img src="images/IconGitHub.svg"></img>';
   popup.style.display = 'block';
 });
-//to close the popub
+// to close the popub
 span.onclick = function () {
-    popup.style.display = 'none';
-}
-//when click anywhere
-window.onclick = function(event) {
+  popup.style.display = 'none';
+};
+// when click anywhere
+window.onclick = function (event) {
   if (event.target === popup) {
-    popup.style.display = "none";
+    popup.style.display = 'none';
   }
-}
+};
 
 const objWorks1 = {
-  title: 'Project1',
+  title: 'Project2',
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: './images/mobileview.svg',
+  image: '',
   technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
   linkSource: 'https//www.githubrepo',
   linkLive: 'https//www.gitpages',
-}
+};
 
-const popup1 = document.getElementById('popup-window');
-//get button that opens the popup
+// const popup1 = document.getElementById('popup-window');
+// get button that opens the popup
 const btn1 = document.getElementById('button1');
-//get span element that closes popup
-//const span = document.getElementsByClassName('close')[0];
-//to open the button on click
-btn1.addEventListener ('click', () => {
+// get span element that closes popup
+// const span = document.getElementsByClassName('close')[0];
+// to open the button on click
+btn1.addEventListener('click', () => {
   const title = document.getElementById('projectTitle');
   title.innerHTML = objWorks1.title;
   const description = document.getElementById('projectDescription');
@@ -83,33 +83,33 @@ btn1.addEventListener ('click', () => {
   button.innerHTML = 'See live <img src="images/IconGitHub.svg"></img>';
   popup.style.display = 'block';
 });
-//to close the popub
+// to close the popub
 span.onclick = function () {
-    popup.style.display = 'none';
-}
-//when click anywhere
-window.onclick = function(event) {
+  popup.style.display = 'none';
+};
+// when click anywhere
+window.onclick = function (event) {
   if (event.target === popup) {
-    popup.style.display = "none";
+    popup.style.display = 'none';
   }
-}
+};
 
 const objWorks2 = {
-  title: 'Project2',
+  title: 'Project3',
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: './images/mobileview.svg',
+  image: '',
   technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
   linkSource: 'https//www.githubrepo',
   linkLive: 'https//www.gitpages',
-}
+};
 
-const popup2 = document.getElementById('popup-window');
-//get button that opens the popup
+// const popup2 = document.getElementById('popup-window');
+// get button that opens the popup
 const btn2 = document.getElementById('button2');
-//get span element that closes popup
-//const span = document.getElementsByClassName('close')[0];
-//to open the button on click
-btn2.addEventListener ('click', () => {
+// get span element that closes popup
+// const span = document.getElementsByClassName('close')[0];
+// to open the button on click
+btn2.addEventListener('click', () => {
   const title = document.getElementById('projectTitle');
   title.innerHTML = objWorks2.title;
   const description = document.getElementById('projectDescription');
@@ -123,37 +123,130 @@ btn2.addEventListener ('click', () => {
   button.innerHTML = 'See live <img src="images/IconGitHub.svg"></img>';
   popup.style.display = 'block';
 });
-//to close the popub
+// to close the popub
 span.onclick = function () {
-    popup.style.display = 'none';
-}
-//when click anywhere
-window.onclick = function(event) {
+  popup.style.display = 'none';
+};
+// when click anywhere
+window.onclick = function (event) {
   if (event.target === popup) {
-    popup.style.display = "none";
+    popup.style.display = 'none';
   }
-}
+};
 const objWorks3 = {
-  title: 'Keeping track of hundreds of components',
+  title: 'Project 4',
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: './images/mobileview.svg',
+  image: '',
   technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
   linkSource: 'https//www.githubrepo',
   linkLive: 'https//www.gitpages',
-}
+};
+
+// const popup3 = document.getElementById('popup-window');
+// get button that opens the popup
+const btn3 = document.getElementById('button3');
+// get span element that closes popup
+// const span = document.getElementsByClassName('close')[0];
+// to open the button on click
+btn3.addEventListener('click', () => {
+  const title = document.getElementById('projectTitle');
+  title.innerHTML = objWorks3.title;
+  const description = document.getElementById('projectDescription');
+  description.innerHTML = objWorks3.description;
+  const image = document.getElementById('projectImage');
+  image.innerHTML = objWorks3.image;
+  const techs = document.getElementById('tags');
+  techs.innerHTML = objWorks3.technologies;
+  const button = document.getElementById('action');
+  button.innerHTML = 'See live <img src="images/Iconlive.svg"></img>';
+  button.innerHTML = 'See live <img src="images/IconGitHub.svg"></img>';
+  popup.style.display = 'block';
+});
+// to close the popub
+span.onclick = function () {
+  popup.style.display = 'none';
+};
+// when click anywhere
+window.onclick = function (event) {
+  if (event.target === popup) {
+    popup.style.display = 'none';
+  }
+};
 const objWorks4 = {
-  title: 'Keeping track of hundreds of components',
+  title: 'Project 5',
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: './images/mobileview.svg',
+  image: '',
   technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
   linkSource: 'https//www.githubrepo',
   linkLive: 'https//www.gitpages',
-}
+};
+
+// const popup4 = document.getElementById('popup-window');
+// get button that opens the popup
+const btn4 = document.getElementById('button4');
+// get span element that closes popup
+// const span = document.getElementsByClassName('close')[0];
+// to open the button on click
+btn4.addEventListener('click', () => {
+  const title = document.getElementById('projectTitle');
+  title.innerHTML = objWorks4.title;
+  const description = document.getElementById('projectDescription');
+  description.innerHTML = objWorks4.description;
+  const image = document.getElementById('projectImage');
+  image.innerHTML = objWorks4.image;
+  const techs = document.getElementById('tags');
+  techs.innerHTML = objWorks4.technologies;
+  const button = document.getElementById('action');
+  button.innerHTML = 'See live <img src="images/Iconlive.svg"></img>';
+  button.innerHTML = 'See live <img src="images/IconGitHub.svg"></img>';
+  popup.style.display = 'block';
+});
+// to close the popub
+span.onclick = function () {
+  popup.style.display = 'none';
+};
+// when click anywhere
+window.onclick = function (event) {
+  if (event.target === popup) {
+    popup.style.display = 'none';
+  }
+};
 const objWorks5 = {
   title: 'Keeping track of hundreds of components',
   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: './images/mobileview.svg',
+  image: '',
   technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
   linkSource: 'https//www.githubrepo',
   linkLive: 'https//www.gitpages',
-}
+};
+
+// const popup5 = document.getElementById('popup-window');
+// get button that opens the popup
+const btn5 = document.getElementById('button5');
+// get span element that closes popup
+// const span = document.getElementsByClassName('close')[0];
+// to open the button on click
+btn5.addEventListener('click', () => {
+  const title = document.getElementById('projectTitle');
+  title.innerHTML = objWorks5.title;
+  const description = document.getElementById('projectDescription');
+  description.innerHTML = objWorks5.description;
+  const image = document.getElementById('projectImage');
+  image.innerHTML = objWorks5.image;
+  const techs = document.getElementById('tags');
+  techs.innerHTML = objWorks5.technologies;
+  const button = document.getElementById('action');
+  button.innerHTML = 'See live <img src="images/Iconlive.svg"></img>';
+  button.innerHTML = 'See live <img src="images/IconGitHub.svg"></img>';
+  popup.style.display = 'block';
+});
+// to close the popub
+span.onclick = function () {
+  popup.style.display = 'none';
+};
+// when click anywhere
+window.onclick = function (event) {
+  if (event.target === popup) {
+    popup.style.display = 'none';
+  }
+};
