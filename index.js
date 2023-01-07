f37cc346b6510dc3ff761b403eb8d23a0c22183f
kiplaacollo <kiplaacollo@gmail.com>
2023-01-07 18:14:02 +0300

    Update Css, index.html and index.js as per code review

--- index.js
+++ index.js
@@ -15,37 +15,83 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
 }));
 
 // Popup details
-const objWorks = {
-  title: 'Keeping track of hundreds of components',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: '',
-  technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
-  linkSource: 'https//www.githubrepo',
-  linkLive: 'https//www.gitpages',
-};
-const popup = document.getElementById('popup-window');
-// get button that opens the popup
-const btn = document.getElementById('button');
-// to open the button on click
-btn.addEventListener('click', () => {
-  const title = document.getElementById('projectTitle');
-  title.innerHTML = objWorks.title;
-  const description = document.getElementById('projectDescription');
-  description.innerHTML = objWorks.description;
-  const image = document.getElementById('projectImage');
-  image.innerHTML = objWorks.image;
-
-  const techs = document.getElementById('tags');
-  for (let i = 0; i < objWorks.technologies.length; i += 1) {
-    techs.innerHTML = `<ul><li><button>${objWorks.technologies[0]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[1]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[2]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[3]}</button></li></ul>`;
+const objWorks = [
+  {
+    title: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: '',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+    linkSource: 'https//www.githubrepo',
+    linkLive: 'https//www.gitpages',
+  },
+    {
+      title: 'Project2',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+      image: '',
+      technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+      linkSource: 'https//www.githubrepo',
+      linkLive: 'https//www.gitpages',
+  },
+  {
+    title: 'Project3',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: '',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+    linkSource: 'https//www.githubrepo',
+    linkLive: 'https//www.gitpages',
+  },
+  {
+    title: 'Project 4',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: '',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+    linkSource: 'https//www.githubrepo',
+    linkLive: 'https//www.gitpages',
+  },
+  {
+    title: 'Keeping track of hundreds of components',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: '',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+    linkSource: 'https//www.githubrepo',
+    linkLive: 'https//www.gitpages',
+  },
+  {
+    title: 'Project 5',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: '',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+    linkSource: 'See Live <img src="images/Iconlive.svg"></img>',
+    linkLive: 'https//www.gitpages',
   }
+];
 
-  popup.style.display = 'block';
-  document.body.style.overflow = 'none';
-});
+const workBox= document.querySelector(".card_Work_Row_1")
+const popup = document.getElementById('popup-window');
+workBox.addEventListener('click',(event)=>{
+  let index=event.target.id
+  // get button that opens the popup
+  const btn = document.getElementById(index);
+  // to open the button on click
+  
+    const title = document.getElementById('projectTitle');
+    title.innerHTML = objWorks[index].title;
+    const description = document.getElementById('projectDescription');
+    description.innerHTML = objWorks[index].description;
+    const image = document.getElementById('projectImage');
+    image.innerHTML = objWorks[index].image;
+  
+    const techs = document.getElementById('tags');
+    for (let i = 0; i < objWorks[index].technologies.length; i += 1) {
+      techs.innerHTML = `<ul><li><button>${objWorks[0].technologies[0]}</button></li></ul>
+                          <ul><li><button>${objWorks[0].technologies[1]}</button></li></ul>
+                          <ul><li><button>${objWorks[0].technologies[2]}</button></li></ul>
+                          <ul><li><button>${objWorks[0].technologies[3]}</button></li></ul>`;
+    }
+  
+    popup.style.display = 'block';
+    document.body.style.overflow = 'none';
+  })
 // get span element that closes popup
 const span = document.getElementById('close');
 // to close the popub
@@ -57,184 +103,4 @@ window.onclick = function (event) {
   if (event.target === popup) {
     popup.style.display = 'none';
   }
-};
-
-const objWorks1 = {
-  title: 'Project2',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: '',
-  technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
-  linkSource: 'https//www.githubrepo',
-  linkLive: 'https//www.gitpages',
-};
-
-const btn1 = document.getElementById('button1');
-
-btn1.addEventListener('click', () => {
-  const title = document.getElementById('projectTitle');
-  title.innerHTML = objWorks1.title;
-  const description = document.getElementById('projectDescription');
-  description.innerHTML = objWorks1.description;
-  const image = document.getElementById('projectImage');
-  image.innerHTML = objWorks1.image;
-
-  const techs = document.getElementById('tags');
-  for (let i = 0; i < objWorks.technologies.length; i += 1) {
-    techs.innerHTML = `<ul><li><button>${objWorks.technologies[0]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[1]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[2]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[3]}</button></li></ul>`;
-  }
-
-  popup.style.display = 'block';
-});
-
-window.onclick = function (event) {
-  if (event.target === popup) {
-    popup.style.display = 'none';
-  }
-};
-
-const objWorks2 = {
-  title: 'Project3',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: '',
-  technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
-  linkSource: 'https//www.githubrepo',
-  linkLive: 'https//www.gitpages',
-};
-
-const btn2 = document.getElementById('button2');
-
-btn2.addEventListener('click', () => {
-  const title = document.getElementById('projectTitle');
-  title.innerHTML = objWorks2.title;
-  const description = document.getElementById('projectDescription');
-  description.innerHTML = objWorks2.description;
-  const image = document.getElementById('projectImage');
-  image.innerHTML = objWorks2.image;
-  const techs = document.getElementById('tags');
-  for (let i = 0; i < objWorks.technologies.length; i += 1) {
-    techs.innerHTML = `<ul><li><button>${objWorks.technologies[0]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[1]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[2]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[3]}</button></li></ul>`;
-  }
-
-  popup.style.display = 'block';
-});
-
-window.onclick = function (event) {
-  if (event.target === popup) {
-    popup.style.display = 'none';
-  }
-};
-const objWorks3 = {
-  title: 'Project 4',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: '',
-  technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
-  linkSource: 'https//www.githubrepo',
-  linkLive: 'https//www.gitpages',
-};
-
-const btn3 = document.getElementById('button3');
-
-btn3.addEventListener('click', () => {
-  const title = document.getElementById('projectTitle');
-  title.innerHTML = objWorks3.title;
-  const description = document.getElementById('projectDescription');
-  description.innerHTML = objWorks3.description;
-  const image = document.getElementById('projectImage');
-  image.innerHTML = objWorks3.image;
-
-  const techs = document.getElementById('tags');
-  for (let i = 0; i < objWorks.technologies.length; i += 1) {
-    techs.innerHTML = `<ul><li><button>${objWorks.technologies[0]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[1]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[2]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[3]}</button></li></ul>`;
-  }
-
-  popup.style.display = 'block';
-});
-
-window.onclick = function (event) {
-  if (event.target === popup) {
-    popup.style.display = 'none';
-  }
-};
-const objWorks4 = {
-  title: 'Project 5',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: '',
-  technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
-  linkSource: 'See Live <img src="images/Iconlive.svg"></img>',
-  linkLive: 'https//www.gitpages',
-};
-
-const btn4 = document.getElementById('button4');
-
-btn4.addEventListener('click', () => {
-  const title = document.getElementById('projectTitle');
-  title.innerHTML = objWorks4.title;
-  const description = document.getElementById('projectDescription');
-  description.innerHTML = objWorks4.description;
-  const image = document.getElementById('projectImage');
-  image.innerHTML = objWorks4.image;
-
-  const techs = document.getElementById('tags');
-  for (let i = 0; i < objWorks.technologies.length; i += 1) {
-    techs.innerHTML = `<ul><li><button>${objWorks.technologies[0]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[1]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[2]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[3]}</button></li></ul>`;
-  }
-
-  popup.style.display = 'block';
-});
-
-window.onclick = function (event) {
-  if (event.target === popup) {
-    popup.style.display = 'none';
-  }
-};
-const objWorks5 = {
-  title: 'Keeping track of hundreds of components',
-  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  image: '',
-  technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
-  linkSource: 'https//www.githubrepo',
-  linkLive: 'https//www.gitpages',
-};
-
-// get button that opens the popup
-const btn5 = document.getElementById('button5');
-// get span element that closes popup
-// to open the button on click
-btn5.addEventListener('click', () => {
-  const title = document.getElementById('projectTitle');
-  title.innerHTML = objWorks5.title;
-  const description = document.getElementById('projectDescription');
-  description.innerHTML = objWorks5.description;
-  const image = document.getElementById('projectImage');
-  image.innerHTML = objWorks5.image;
-
-  const techs = document.getElementById('tags');
-  for (let i = 0; i < objWorks.technologies.length; i += 1) {
-    techs.innerHTML = `<ul><li><button>${objWorks.technologies[0]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[1]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[2]}</button></li></ul>
-                        <ul><li><button>${objWorks.technologies[3]}</button></li></ul>`;
-  }
-
-  popup.style.display = 'block';
-});
-// to close the popub
-
-// when click anywhere
-window.onclick = function (event) {
-  if (event.target === popup) {
-    popup.style.display = 'none';
-  }
-};
+}
