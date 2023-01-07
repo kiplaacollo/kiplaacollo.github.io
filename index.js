0152758c998514045ec47aab8d154e22e5815374
kiplaacollo <kiplaacollo@gmail.com>
2023-01-07 18:26:21 +0300

    Update Css and index.js file fixing errors

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ const objWorks = [
     linkSource: 'https//www.githubrepo',
     linkLive: 'https//www.gitpages',
   },
-    {
-      title: 'Project2',
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-      image: '',
-      technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
-      linkSource: 'https//www.githubrepo',
-      linkLive: 'https//www.gitpages',
+  {
+    title: 'Project2',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    image: '',
+    technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
+    linkSource: 'https//www.githubrepo',
+    linkLive: 'https//www.gitpages',
   },
   {
     title: 'Project3',
@@ -63,35 +63,32 @@ const objWorks = [
     technologies: ['Ruby on Rails', 'css', 'JavaScript', 'html'],
     linkSource: 'See Live <img src="images/Iconlive.svg"></img>',
     linkLive: 'https//www.gitpages',
-  }
+  },
 ];
 
-const workBox= document.querySelector(".card_Work_Row_1")
+const workBox = document.querySelector('.card_Work_Row_1');
 const popup = document.getElementById('popup-window');
-workBox.addEventListener('click',(event)=>{
-  let index=event.target.id
-  // get button that opens the popup
-  const btn = document.getElementById(index);
-  // to open the button on click
-  
-    const title = document.getElementById('projectTitle');
-    title.innerHTML = objWorks[index].title;
-    const description = document.getElementById('projectDescription');
-    description.innerHTML = objWorks[index].description;
-    const image = document.getElementById('projectImage');
-    image.innerHTML = objWorks[index].image;
-  
-    const techs = document.getElementById('tags');
-    for (let i = 0; i < objWorks[index].technologies.length; i += 1) {
-      techs.innerHTML = `<ul><li><button>${objWorks[0].technologies[0]}</button></li></ul>
+workBox.addEventListener('click', (event) => {
+  const index = event.target.id;
+
+  const title = document.getElementById('projectTitle');
+  title.innerHTML = objWorks[index].title;
+  const description = document.getElementById('projectDescription');
+  description.innerHTML = objWorks[index].description;
+  const image = document.getElementById('projectImage');
+  image.innerHTML = objWorks[index].image;
+
+  const techs = document.getElementById('tags');
+  for (let i = 0; i < objWorks[index].technologies.length; i += 1) {
+    techs.innerHTML = `<ul><li><button>${objWorks[0].technologies[0]}</button></li></ul>
                           <ul><li><button>${objWorks[0].technologies[1]}</button></li></ul>
                           <ul><li><button>${objWorks[0].technologies[2]}</button></li></ul>
                           <ul><li><button>${objWorks[0].technologies[3]}</button></li></ul>`;
-    }
-  
-    popup.style.display = 'block';
-    document.body.style.overflow = 'none';
-  })
+  }
+
+  popup.style.display = 'block';
+  document.body.style.overflow = 'none';
+});
 // get span element that closes popup
 const span = document.getElementById('close');
 // to close the popub
@@ -103,4 +100,4 @@ window.onclick = function (event) {
   if (event.target === popup) {
     popup.style.display = 'none';
   }
-}
+};
